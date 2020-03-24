5c7c9a999626f0e5bf1c9c033a39b6dd54855119
solimano <solimano@MacBook-Pro-de-solimano.local>
2020-03-24 07:21:57 -0500

    feat/subfinal

--- controller/ApisController.js
+++ controller/ApisController.js
@@ -6,15 +6,65 @@ import request from 'request';
 export default {
 
 
-listTest:async(req,res,next)=>{
+api_one:async(req,res,next)=>{
 	try{
 	//	let valor=req.query.valor;
         request('https://api.humanapi.co/v1/human/sleeps?access_token=demo', function(err, body){
          
             res.status(200).json(body);
         });
+	}catch(e){
+		res.status(500).send({ message:'Ocurrio un error'});
+		next(e);
+	}	
 
+},
+api_two:async(req,res,next)=>{
+	try{
+	//	let valor=req.query.valor;
+        request('https://api.humanapi.co/v1/human/sleeps?access_token=demo', function(err, body){
+         
+            res.status(200).json(body);
+        });
+	}catch(e){
+		res.status(500).send({ message:'Ocurrio un error'});
+		next(e);
+	}	
 
+},
+api_three:async(req,res,next)=>{
+	try{
+	//	let valor=req.query.valor;
+        request('https://api.humanapi.co/v1/human/sleeps?access_token=demo', function(err, body){
+         
+            res.status(200).json(body);
+        });
+	}catch(e){
+		res.status(500).send({ message:'Ocurrio un error'});
+		next(e);
+	}	
+
+},
+api_four:async(req,res,next)=>{
+	try{
+	//	let valor=req.query.valor;
+        request('https://api.humanapi.co/v1/human/sleeps?access_token=demo', function(err, body){
+         
+            res.status(200).json(body);
+        });
+	}catch(e){
+		res.status(500).send({ message:'Ocurrio un error'});
+		next(e);
+	}	
+
+},
+api_five:async(req,res,next)=>{
+	try{
+	//	let valor=req.query.valor;
+        request('https://api.humanapi.co/v1/human/sleeps?access_token=demo', function(err, body){
+         
+            res.status(200).json(body);
+        });
 	}catch(e){
 		res.status(500).send({ message:'Ocurrio un error'});
 		next(e);
--- routes/apis.js
+++ routes/apis.js
@@ -3,14 +3,9 @@ import apisController from '../controller/ApisController';
 import auth from '../middlewares/auth';
 const router=routerx();
 
-
-// router.post('/add',auth.verifyAdministrador,usuarioController.add);
-// router.get('/query',auth.verifyAdministrador,usuarioController.query);
-// router.get('/list',auth.verifyAdministrador,usuarioController.list);
-// router.put('/update',auth.verifyAdministrador,usuarioController.update);
-// router.delete('/remove',auth.verifyAdministrador,usuarioController.remove);
-// router.put('/activate',auth.verifyAdministrador,usuarioController.activate);
-// router.put('/desactivate',auth.verifyAdministrador,usuarioController.desactivate);
-router.post('/test1',auth.verifyAdministrador,apisController.listTest);
-
+router.get('/api_one',auth.verifyAdministrador,apisController.api_one);
+router.get('/api_two',auth.verifyAdministrador,apisController.api_two);
+router.get('/api_three',auth.verifyUserGeneric,apisController.api_three);
+router.get('/api_four',auth.verifyUserGeneric,apisController.api_four);
+router.get('/api_five',auth.verifyUserGeneric,apisController.api_five);
 export default router;
